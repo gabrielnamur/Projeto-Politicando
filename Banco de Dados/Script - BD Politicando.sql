d22c8e544f2ef20c1ec1176a92fc64a642068a0a
gabrielnamur <gabriel.msilva@sptech.school>
2023-05-15 18:21:08 -0300

    Realizei alterações diversas.

--- Banco de Dados/Script - BD Politicando.sql
+++ Banco de Dados/Script - BD Politicando.sql
@@ -5,12 +5,14 @@ USE politicando;
 CREATE TABLE Usuário (
 idUsuario INT PRIMARY KEY auto_increment,
 nome VARCHAR(100),
-telefone CHAR(15),
 sexo CHAR(1),
 email VARCHAR(100),
 senha VARCHAR(45),
 confirmacaosenha VARCHAR(45)
 );
+
+INSERT INTO Usuário VALUES 
+	(1, 'Gabriel', 'M', 'namur_gabriel@yahoo.com', '123', '123');
 
 CREATE TABLE Questionário (
 idQuestionario INT PRIMARY KEY,
@@ -18,8 +20,20 @@ resultado VARCHAR(50),
 fkUsuario INT 
 );
 
-CREATE TABLE Newsletter (
-idNewsletter INT PRIMARY KEY,
-nome_deputado_usario VARCHAR(45),
+INSERT INTO Questionário VALUES	
+	(1, 'Esquerda', 1),
+    (2, 'Centro-esquerda', 2),
+    (3, 'Centro', 3),
+    (4, 'Direita', 4),
+    (5, 'Extrema-direita', 5);
+
+CREATE TABLE Votação (
+idVotação INT PRIMARY KEY,
+deputado_escolhido 	VARCHAR(100),
+voto_usuario CHAR(1),
 fkUsuario INT
 );
+
+INSERT INTO Votação VALUES 
+	(1, 'Tabata Amaral', 'S', 1),
+    (2, 'Kim Kataguiri', 'N', 2);
