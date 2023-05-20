ea4d700eea7dd96b6aee509e3741e038d5a62e4e
gabrielnamur <gabriel.msilva@sptech.school>
2023-05-20 01:20:59 -0300

    Adicionei a API web-data-viz ao site.

---
+++ site/src/database/Script - BD Politicando.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE politicando;
+
+USE politicando;
+
+CREATE TABLE Usuário (
+idUsuario INT PRIMARY KEY auto_increment,
+nome VARCHAR(100),
+sexo CHAR(1),
+email VARCHAR(100),
+senha VARCHAR(45)
+);
+
+INSERT INTO Usuário VALUES 
+	(1, 'Gabriel', 'M', 'namur_gabriel@yahoo.com', '123', '123');
+
+CREATE TABLE Questionário (
+idQuestionario INT PRIMARY KEY,
+resultado VARCHAR(50),
+fkUsuario INT 
+);
+
+INSERT INTO Questionário VALUES	
+	(1, 'Esquerda', 1),
+    (2, 'Centro-esquerda', 2),
+    (3, 'Centro', 3),
+    (4, 'Direita', 4),
+    (5, 'Extrema-direita', 5);
+
+CREATE TABLE Votação (
+idVotação INT PRIMARY KEY,
+deputado_escolhido 	VARCHAR(100),
+voto_usuario CHAR(1),
+fkUsuario INT
+);
+
+INSERT INTO Votação VALUES 
+	(1, 'Tabata Amaral', 'S', 1),
+    (2, 'Kim Kataguiri', 'N', 2);
